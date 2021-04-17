6c37321ca6fe1ca22317ab2134ac81ce71a0180a
AbhiRam0099 <52668950+AbhiRam0099@users.noreply.github.com>
2021-04-16 22:56:05 -0500

    9b added

---
+++ Question9b/src/question9/Question9.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question9;
+
+/**
+ *
+ * @author Abhilash Ramavaram
+ */
+public class Question9 {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+  
+// A class that implements the interface.
+
+    // Driver Code
+    public static void main (String[] args)
+    {
+        amazonbox a = new amazonbox();
+    a.getArea(6, 5);
+    }
+}
+    
+
---
+++ Question9b/src/question9/sub.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question9;
+
+/**
+ *
+ * @author Abhilash Ramavaram
+ */
+class amazonbox implements I
+{
+public void getArea(int length, int breadth) {
+    System.out.println("The area of the amazonbox is " + (length * breadth));
+  }
+}
